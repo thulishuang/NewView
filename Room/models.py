257cf2374f6c5fd5c6d80d842109ee29cad5f3ee
willispyoung <willispyoung@gmail.com>
2016-12-28 14:48:29 +0800

    change to django User model and finish APIs

--- Room/models.py
+++ Room/models.py
@@ -4,7 +4,7 @@ from Account.models import Interviewee, Interviewer
 
 
 class Room(models.Model):
-    # url = models.CharField(max_length=200, default="")
+    port = models.IntegerField(default=8000, unique=True)
     title = models.CharField(max_length=200, default="Default room title")
     description = models.CharField(max_length=1000, default="Default description for a default room")
     state = models.BooleanField(default=False)
